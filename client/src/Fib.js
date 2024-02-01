4fcc3fc289ca3e15ddd5263a30456eef1f7a995d
Anton Zotov <votoznotna@gmail.com>
2024-01-31 16:27:46 -0800

    excluded aws deployment and added exception prevention

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -1,25 +1,27 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 class Fib extends Component {
   state = {
     seenIndexes: [],
     values: {},
-    index: '',
+    index: "",
   };
 
   componentDidMount() {
-    this.fetchValues();
-    this.fetchIndexes();
+    try {
+      this.fetchValues();
+      this.fetchIndexes();
+    } catch {}
   }
 
   async fetchValues() {
-    const values = await axios.get('/api/values/current');
+    const values = await axios.get("/api/values/current");
     this.setState({ values: values.data });
   }
 
   async fetchIndexes() {
-    const seenIndexes = await axios.get('/api/values/all');
+    const seenIndexes = await axios.get("/api/values/all");
     this.setState({
       seenIndexes: seenIndexes.data,
     });
@@ -28,14 +30,14 @@ class Fib extends Component {
   handleSubmit = async (event) => {
     event.preventDefault();
 
-    await axios.post('/api/values', {
+    await axios.post("/api/values", {
       index: this.state.index,
     });
-    this.setState({ index: '' });
+    this.setState({ index: "" });
   };
 
   renderSeenIndexes() {
-    return this.state.seenIndexes.map(({ number }) => number).join(', ');
+    return this.state.seenIndexes.map(({ number }) => number).join(", ");
   }
 
   renderValues() {
